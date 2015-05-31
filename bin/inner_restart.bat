d08e1f40649f64d5b84c706481c4989c0857a2ed
chenxingbin <chenxingbin@ctsi.com.cn>
2015-05-31 11:53:48 +0800

    inner_restart command

--- bin/inner_restart.bat
+++ bin/inner_restart.bat
@@ -3,11 +3,16 @@ call set_env.bat
 
 cd "%RS_HOME%\system\apache-tomcat-6.0.26\bin\"
 call catalina.bat stop
-set CLASSPATH=%CLASSPATH%:%JAVA_HOME%/lib/tools.jar:%JAVA_HOME%/lib/dt.jar:.
-set PATH=%path%;%JAVA_HOME%/bin
-call %RS_HOME%/system/apache-ant-1.8.1/bin/ant -f "%RS_HOME%\src\webapp\build.xml" deploy
+set CLASSPATH=%CLASSPATH%:%JAVA_HOME%\lib\tools.jar:%JAVA_HOME%\lib\dt.jar:.
+set PATH=%path%;%JAVA_HOME%\bin
+cd %RS_HOME%\src\project\
+call %RS_HOME%\system\apache-maven-3.2.5\bin/mvn.bat clean install
+copy target\rs.war "%RS_HOME%\system\apache-tomcat-6.0.26\"
+cd "%RS_HOME%\system\apache-tomcat-6.0.26\"
+call "%RS_HOME%\system\WinRAR\WinRAR.exe e rs.war "%RS_HOME%\system\apache-tomcat-6.0.26\rs"
 set JAVA_OPTS=-Xms512m -Xmx1024m
 set CATALINA_OPTS=-Xms512m -Xmx1024m
+cd "%RS_HOME%\system\apache-tomcat-6.0.26\bin\"
 call catalina.bat start
 
-cd "%RS_HOME%\bin"
+cd "%RS_HOME%\src\bin"
